fb23c83f10efdfd6273e16196997164b5c6b45a4
Joshua Hardin <joshhardinmusic@gmail.com>
2023-11-21 17:18:05 -0600

    done

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,23 @@
-// Your code here 
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons) {
+    let allDragons = []
+
+    for (let dragon of dragons) {
+     allDragons.push(dragon.name)
+    }
+    return allDragons;
+  }
+
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
